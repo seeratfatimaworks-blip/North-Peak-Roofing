6bd0999e3aa71200ee7f8701def79e37d13b40f9
Seerat <seeratfatimaworks@gmail.com>
2026-09-09 06:29:31 +0100

    Deploy Express API on Vercel

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ app.get("/", (req, res) => {
 
 app.use("/api/leads", leadRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
